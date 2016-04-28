bccb4a8437d95a7e7dfd9f0b72e27021edf8c124
Christoffer Dall <christoffer.dall@arm.com>
2016-04-28 22:10:47 +0200

    KVM: arm64: nv: Add nested virt VCPU primitives for vEL2 VCPU state
    When running a nested hypervisor we commonly have to figure out if
    the VCPU mode is running in the context of a guest hypervisor or guest
    guest, or just a normal guest.
    
    Add convenient primitives for this.
    
    Signed-off-by: Christoffer Dall <christoffer.dall@arm.com>

--- arch/arm/include/asm/kvm_nested.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef __ARM_KVM_NESTED_H
-#define __ARM_KVM_NESTED_H
-
-#include <linux/kvm_host.h>
-
-static inline bool nested_virt_in_use(const struct kvm_vcpu *vcpu) { return false; }
-
-#endif /* __ARM_KVM_NESTED_H */

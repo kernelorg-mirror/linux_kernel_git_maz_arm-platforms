a2b7fbbba10c8913abf5a1becd2c7f16c3de6043
Christoffer Dall <christoffer.dall@linaro.org>
2016-05-07 19:42:29 +0200

    KVM: arm64: Fixes to toggle_cache for nesting
    So far we were flushing almost the entire universe whenever a VM would
    load/unload the SCTLR_EL1 and the two versions of that register had
    different MMU enabled settings.  This turned out to be so slow that it
    prevented forward progress for a nested VM, because a scheduler timer
    tick interrupt would always be pending when we reached the nested VM.
    
    To avoid this problem, we consider the SCTLR_EL2 when evaluating if
    caches are on or off when entering virtual EL2 (because this is the
    value that we end up shadowing onto the hardware EL1 register).
    
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>

--- arch/arm64/include/asm/kvm_mmu.h
+++ arch/arm64/include/asm/kvm_mmu.h
@@ -274,7 +274,10 @@ struct kvm;
 
 static inline bool vcpu_has_cache_enabled(struct kvm_vcpu *vcpu)
 {
-	return (vcpu_read_sys_reg(vcpu, SCTLR_EL1) & 0b101) == 0b101;
+	if (vcpu_mode_el2(vcpu))
+		return (__vcpu_sys_reg(vcpu, SCTLR_EL2) & 0b101) == 0b101;
+	else
+		return (vcpu_read_sys_reg(vcpu, SCTLR_EL1) & 0b101) == 0b101;
 }
 
 static inline void __clean_dcache_guest_page(kvm_pfn_t pfn, unsigned long size)

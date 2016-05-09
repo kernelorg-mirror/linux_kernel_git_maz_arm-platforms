8c18c5ca9cf1cfb0bf53df38a57c2cfee7aeb2b8
Jintack Lim <jintack@cs.columbia.edu>
2016-05-09 12:23:13 -0600

    KVM: arm/arm64: Create a vcpu mapping for the nested VM
    Create a mapping from the nested VM's cpu interface to the hardware
    virtual cpu interface. This is to allow the nested VM to access virtual
    cpu interface directly.
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- arch/arm64/kvm/mmu-nested.c
+++ arch/arm64/kvm/mmu-nested.c
@@ -439,3 +439,27 @@ struct kvm_s2_mmu *vcpu_get_active_s2_mmu(struct kvm_vcpu *vcpu)
 
 	return get_s2_mmu_nested(vcpu);
 }
+
+int kvm_nested_mmio_ondemand(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
+			     phys_addr_t ipa)
+{
+	int ret = 0;
+	phys_addr_t vcpu_base = vgic_vcpu_base();
+
+	if (!nested_virt_in_use(vcpu))
+		return 0;
+
+	/* Return if this fault is not from a nested VM */
+	if (vcpu->arch.hw_mmu == &vcpu->kvm->arch.mmu)
+		return ret;
+
+	if (ipa == vcpu->kvm->arch.vgic.vgic_vcpu_base)  {
+		ret = __kvm_phys_addr_ioremap(vcpu->kvm, vcpu->arch.hw_mmu,
+					      fault_ipa, vcpu_base,
+					      KVM_VGIC_V2_VCPU_SIZE, true);
+		if (!ret)
+			ret = 1;
+	}
+
+	return ret;
+}

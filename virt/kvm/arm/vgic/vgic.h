f6c23091dfa4ef98392f8e7dca71491586522058
Jintack Lim <jintack@cs.columbia.edu>
2016-12-02 22:05:48 -0500

    KVM: arm64: nv: Nested GICv3 Support
    When entering a nested VM, we set up the hypervisor control interface
    based on what the guest hypervisor has set. Especially, we investigate
    each list register written by the guest hypervisor whether HW bit is
    set.  If so, we translate hw irq number from the guest's point of view
    to the real hardware irq number if there is a mapping.
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>
    [Rewritten to support GICv3 instead of GICv2]
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    [Redesigned execution flow around vcpu load/put]
    Signed-off-by: Christoffer Dall <christoffer.dall@arm.com>

--- virt/kvm/arm/vgic/vgic.h
+++ virt/kvm/arm/vgic/vgic.h
@@ -17,6 +17,7 @@
 #define __KVM_ARM_VGIC_NEW_H__
 
 #include <linux/irqchip/arm-gic-common.h>
+#include <asm/kvm_emulate.h>
 
 #define PRODUCT_ID_KVM		0x4b	/* ASCII code K */
 #define IMPLEMENTER_ARM		0x43b

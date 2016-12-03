273945b25a6e98ee9aaed1c919d31ffada0f6354
Jintack Lim <jintack@cs.columbia.edu>
2016-12-02 22:05:48 -0500

    KVM: arm64: nv: Nested GICv3 Support
    When entering a nested VM, we set up the hypervisor control interface
    based on what the guest hypervisor has set. Especially, we investigate
    each list register written by the guest hypervisor whether HW bit is
    set.  If so, we translate hw irq number from the guest's point of view
    to the real hardware irq number if there is a mapping.
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>
    [Rewritten to support GICv3 instead of GICv2]
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    [Redesigned execution flow around vcpu load/put]
    Signed-off-by: Christoffer Dall <christoffer.dall@arm.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -698,6 +698,8 @@ static void check_vcpu_requests(struct kvm_vcpu *vcpu)
 			vgic_v4_load(vcpu);
 			preempt_enable();
 		}
+
+		check_nested_vcpu_requests(vcpu);
 	}
 }
 
@@ -744,9 +746,16 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu)
 		 */
 		cond_resched();
 
-		update_vmid(&vcpu->arch.hw_mmu->vmid);
-
+		/*
+		 * A nested exeption triggered by a vcpu request (such
+		 * as an interrupt injected in a guest hypervisor) can
+		 * change the currently used VMID (by switching to a
+		 * different translation regime. It is thus necesary
+		 * to update the VMID *after* all requests have been
+		 * processed.
+		 */
 		check_vcpu_requests(vcpu);
+		update_vmid(&vcpu->arch.hw_mmu->vmid);
 
 		/*
 		 * Preparing the interrupts to be injected also

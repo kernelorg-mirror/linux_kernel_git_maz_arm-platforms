b4fd6fb6d209a48fedf88d947556637253bdf590
Jintack Lim <jintack@cs.columbia.edu>
2016-12-03 13:07:49 -0500

    KVM: arm64: Expose physical address of vcpu interface
    Expose physical address of vgic virtual cpu interface.
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>
    
    Conflicts:
    	virt/kvm/arm/vgic/vgic-v2.c

--- virt/kvm/arm/vgic/vgic-v2.c
+++ virt/kvm/arm/vgic/vgic-v2.c
@@ -509,3 +509,9 @@ void vgic_v2_put(struct kvm_vcpu *vcpu)
 	cpu_if->vgic_vmcr = readl_relaxed(kvm_vgic_global_state.vctrl_base + GICH_VMCR);
 	cpu_if->vgic_apr = readl_relaxed(kvm_vgic_global_state.vctrl_base + GICH_APR);
 }
+
+/* Return physical address of vgic virtual cpu interface */
+phys_addr_t vgic_vcpu_base(void)
+{
+	return kvm_vgic_global_state.vcpu_base;
+}

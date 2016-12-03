847286af69640965d1a34e1a2cc2cd1cef649de6
Marc Zyngier <maz@kernel.org>
2016-12-02 22:05:48 -0500

    KVM: arm64: nv: Nested GICv3 Support
    When entering a nested VM, we set up the hypervisor control interface
    based on what the guest hypervisor has set. Especially, we investigate
    each list register written by the guest hypervisor whether HW bit is
    set.  If so, we translate hw irq number from the guest's point of view
    to the real hardware irq number if there is a mapping.
    
    Co-developed-by: Jintack Lim <jintack@cs.columbia.edu>
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>
    [Christoffer: Redesigned execution flow around vcpu load/put]
    Co-developed-by: Christoffer Dall <christoffer.dall@arm.com>
    Signed-off-by: Christoffer Dall <christoffer.dall@arm.com>
    [maz: Rewritten to support GICv3 instead of GICv2, NV2 support]
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/vgic/vgic-v3.c
+++ arch/arm64/kvm/vgic/vgic-v3.c
@@ -735,6 +735,12 @@ void vgic_v3_load(struct kvm_vcpu *vcpu)
 {
 	struct vgic_v3_cpu_if *cpu_if = &vcpu->arch.vgic_cpu.vgic_v3;
 
+	/* If the vgic is nested, perform the full state loading */
+	if (vgic_state_is_nested(vcpu)) {
+		vgic_v3_load_nested(vcpu);
+		return;
+	}
+
 	vcpu->arch.vgic_cpu.current_cpu_if = cpu_if;
 
 	kvm_call_hyp(__vgic_v3_restore_vmcr_aprs, cpu_if);
@@ -748,6 +754,11 @@ void vgic_v3_load(struct kvm_vcpu *vcpu)
 void vgic_v3_put(struct kvm_vcpu *vcpu)
 {
 	struct vgic_v3_cpu_if *cpu_if = &vcpu->arch.vgic_cpu.vgic_v3;
+
+	if (vgic_state_is_nested(vcpu)) {
+		vgic_v3_put_nested(vcpu);
+		return;
+	}
 
 	kvm_call_hyp(__vgic_v3_save_vmcr_aprs, cpu_if);
 	WARN_ON(vgic_v4_put(vcpu));

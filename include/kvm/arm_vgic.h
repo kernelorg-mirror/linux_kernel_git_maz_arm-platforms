a528c9e903228ced99ab50ff8589a5bb8826f6e5
Jintack Lim <jintack@cs.columbia.edu>
2016-12-03 15:38:00 -0500

    KVM: arm/arm64: register GICH iodev for the guest hypervisor
    Register a device for the virtual interface control block(GICH) access
    from the guest hypervisor.
    
    TODO: Get GICH address from DT, which is hardcoded now.
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>
    
    Conflicts:
    	virt/kvm/arm/vgic/vgic.h

--- include/kvm/arm_vgic.h
+++ include/kvm/arm_vgic.h
@@ -158,7 +158,8 @@ enum iodev_type {
 	IODEV_CPUIF,
 	IODEV_DIST,
 	IODEV_REDIST,
-	IODEV_ITS
+	IODEV_ITS,
+	IODEV_GICH,
 };
 
 struct vgic_io_device {
@@ -248,6 +249,8 @@ struct vgic_dist {
 	struct vgic_irq		*spis;
 
 	struct vgic_io_device	dist_iodev;
+
+	struct vgic_io_device	hyp_iodev;
 
 	bool			has_its;
 

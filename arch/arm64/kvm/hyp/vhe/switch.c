e0033fda5b2b860304ccb4c17c1955fd655ef59b
Jintack Lim <jintack.lim@linaro.org>
2016-12-28 11:43:20 -0500

    KVM: arm64: nv: Trap CPACR_EL1 access in virtual EL2
    For the same reason we trap virtual memory register accesses in virtual
    EL2, we trap CPACR_EL1 access too; We allow the virtual EL2 mode to
    access EL1 system register state instead of the virtual EL2 one.
    
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/hyp/vhe/switch.c
+++ arch/arm64/kvm/hyp/vhe/switch.c
@@ -62,6 +62,9 @@ static void __activate_traps(struct kvm_vcpu *vcpu)
 		__activate_traps_fpsimd32(vcpu);
 	}
 
+	if (vcpu_mode_el2(vcpu) && !vcpu_el2_e2h_is_set(vcpu))
+		val |= CPTR_EL2_TCPAC;
+	
 	write_sysreg(val, cpacr_el1);
 
 	write_sysreg(kvm_get_hyp_vector(), vbar_el1);

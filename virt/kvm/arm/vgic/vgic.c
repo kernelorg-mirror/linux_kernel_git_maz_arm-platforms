5603a488908713e2517e33fb543ae2d1d062b893
Jintack Lim <jintack@cs.columbia.edu>
2017-06-12 14:19:10 -0400

    KVM: arm64 Check LRs instead of AP list for L1 irq injection
    We used to check vgic_irq in the AP list to see if we need to inject an
    irq to L1.  However, at this point, all the informations are flushed to
    LRs.  So, let's look at LRs instead.
    
    This fixes a problem that L0 doesn't inject IPIs to L1 even though it
    has one for L1. This is because when populating LRs, the pending bit of
    an edge sensitive IRQ (including IPI) is cleared in struct vgic_irq.
    
    TODO: check the pending condition in the loop rather than after the loop.
    list_for_each_entry(irq, &vgic_cpu->ap_list_head, ap_list)
    
    Signed-off-by: Jintack Lim <jintack@cs.columbia.edu>

--- virt/kvm/arm/vgic/vgic.c
+++ virt/kvm/arm/vgic/vgic.c
@@ -21,6 +21,7 @@
 #include <linux/list_sort.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
+#include <linux/irqchip/arm-gic.h>
 #include <linux/nospec.h>
 #include <asm/kvm_emulate.h>
 #include <asm/kvm_hyp.h>
@@ -728,6 +729,15 @@ static inline void vgic_set_underflow(struct kvm_vcpu *vcpu)
 		vgic_v3_set_underflow(vcpu);
 }
 
+static inline u32 vgic_get_lr(struct kvm_vcpu *vcpu, int lr)
+{
+	if (kvm_vgic_global_state.type == VGIC_V2)
+		return vgic_v2_get_lr(vcpu, lr);
+
+	// TODO: implement vgic_v3_get_lr(). This is 64 bit
+	return 0;
+}
+
 /* Requires the ap_list_lock to be held. */
 static int compute_ap_list_depth(struct kvm_vcpu *vcpu,
 				 bool *multi_sgi)
@@ -762,6 +772,7 @@ static void vgic_flush_lr_state(struct kvm_vcpu *vcpu)
 	int count;
 	bool multi_sgi;
 	u8 prio = 0xff;
+	int i = 0;
 
 	DEBUG_SPINLOCK_BUG_ON(!spin_is_locked(&vgic_cpu->ap_list_lock));
 
@@ -817,14 +828,9 @@ static void vgic_flush_lr_state(struct kvm_vcpu *vcpu)
 	 * means an irq of which state is pending but not active.
 	 */
 	if (vcpu_el2_imo_is_set(vcpu) && !vcpu_mode_el2(vcpu)) {
-		bool pending = false;
-
-		list_for_each_entry(irq, &vgic_cpu->ap_list_head, ap_list) {
-			spin_lock(&irq->irq_lock);
-			pending = irq_is_pending(irq) && irq->enabled && !irq->active;
-			spin_unlock(&irq->irq_lock);
-
-			if (pending) {
+		for (i = 0; i < vcpu->arch.vgic_cpu.used_lrs; i++) {
+			u32 vgic_lr = vgic_get_lr(vcpu, i);
+			if ((GICH_LR_PENDING_BIT & vgic_lr) && (!(GICH_LR_ACTIVE_BIT & vgic_lr))) {
 				kvm_inject_nested_irq(vcpu);
 				break;
 			}

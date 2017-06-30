16ead28d63be686268c5a5e82db52ec99cb17c08
Jintack Lim <jintack.lim@linaro.org>
2017-06-29 22:07:23 -0500

    KVM: arm64: nv: Trap SPSR_EL1, ELR_EL1 and VBAR_EL1 from virtual EL2
    For the same reason we trap virtual memory register accesses at virtual
    EL2, we need to trap SPSR_EL1, ELR_EL1 and VBAR_EL1 accesses. ARM v8.3
    introduces the HCR_EL2.NV1 bit to be able to trap on those register
    accesses in EL1. Do not set this bit until the whole nesting support is
    completed.
    
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -202,6 +202,9 @@
 #define SYS_APGAKEYLO_EL1		sys_reg(3, 0, 2, 3, 0)
 #define SYS_APGAKEYHI_EL1		sys_reg(3, 0, 2, 3, 1)
 
+#define	SYS_SPSR_EL1			sys_reg(3, 0, 4, 0, 0)
+#define	SYS_ELR_EL1			sys_reg(3, 0, 4, 0, 1)
+
 #define SYS_ICC_PMR_EL1			sys_reg(3, 0, 4, 6, 0)
 
 #define SYS_AFSR0_EL1			sys_reg(3, 0, 5, 1, 0)

867fbc581ecc2405a667356a35207561c62f4832
Jintack Lim <jintack.lim@linaro.org>
2017-07-17 13:27:14 -0500

    KVM: arm64: nv: Respect the virtual HCR_EL2.NV bit setting
    Forward traps due to HCR_EL2.NV bit to the virtual EL2 if they are not
    coming from the virtual EL2 and the virtual HCR_EL2.NV bit is set.
    
    In addition to EL2 register accesses, setting NV bit will also make EL12
    register accesses trap to EL2. To emulate this for the virtual EL2,
    forword traps due to EL12 register accessses to the virtual EL2 if the
    virtual HCR_EL2.NV bit is set. Same thing is done for SMCs issued by
    a guest and trapped by the virtual EL2.
    
    This is for recursive nested virtualization.
    
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>
    [Moved code to emulate-nested.c]
    Signed-off-by: Christoffer Dall <christoffer.dall@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/kvm_nested.h
+++ arch/arm64/include/asm/kvm_nested.h
@@ -59,4 +59,7 @@ static inline u64 translate_ttbr0_el2_to_ttbr0_el1(u64 ttbr0)
 	return ttbr0 & ~GENMASK_ULL(63, 48);
 }
 
+extern bool forward_traps(struct kvm_vcpu *vcpu, u64 control_bit);
+extern bool forward_nv_traps(struct kvm_vcpu *vcpu);
+
 #endif /* __ARM64_KVM_NESTED_H */

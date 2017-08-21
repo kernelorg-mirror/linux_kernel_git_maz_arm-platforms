203ec9fedf1447a2d050373f435bb662a653affa
Marc Zyngier <marc.zyngier@arm.com>
2017-08-21 18:15:41 +0100

    PCI/MSI: Move MSI domain discovery to the allocation point
    Now that we're able to discover the MSI domain in a more "lazy" way,
    let's stop populating the msi_domain field in the probing code, and
    instead assign it when we actually need it, in the PCI/MSI code.
    
    For this, we introduce a new helper (pci_get_msi_domain), which does
    the heavy lifting of walking the PCI hierarchy and return the domain
    (or NULL if none can be found).
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -820,7 +820,6 @@ static int pci_register_host_bridge(struct pci_host_bridge *bridge)
 	bus->bridge = get_device(&bridge->dev);
 	device_enable_async_suspend(bus->bridge);
 	pci_set_bus_of_node(bus);
-	pci_set_bus_msi_domain(bus);
 
 	if (!parent)
 		set_dev_node(bus->bridge, pcibus_to_node(bus));
@@ -984,7 +983,6 @@ static struct pci_bus *pci_alloc_child_bus(struct pci_bus *parent,
 	bridge->subordinate = child;
 
 add_dev:
-	pci_set_bus_msi_domain(child);
 	ret = device_register(&child->dev);
 	WARN_ON(ret < 0);
 
@@ -2414,6 +2412,19 @@ static void pci_set_msi_domain(struct pci_dev *dev)
 	dev_set_msi_domain(&dev->dev, d);
 }
 
+struct irq_domain *pci_get_msi_domain(struct pci_dev *dev)
+{
+	struct irq_domain *d;
+
+	d = pci_dev_msi_domain(dev);
+	if (!d) {
+		pci_set_msi_domain(dev);
+		d = pci_dev_msi_domain(dev);
+	}
+
+	return d;
+}
+
 void pci_device_add(struct pci_dev *dev, struct pci_bus *bus)
 {
 	int ret;
@@ -2453,9 +2464,6 @@ void pci_device_add(struct pci_dev *dev, struct pci_bus *bus)
 
 	ret = pcibios_add_device(dev);
 	WARN_ON(ret < 0);
-
-	/* Set up MSI IRQ domain */
-	pci_set_msi_domain(dev);
 
 	/* Notifier could use PCI capabilities */
 	dev->match_driver = false;

6549398e6d2fc373f10210cec2e6aa2c13f2bcbd
Marc Zyngier <marc.zyngier@arm.com>
2017-08-21 17:49:28 +0100

    of/platform: Do not populate platform device msi_domain
    Now that the core platform MSI code is able to deal with populating
    the device MSI domain, let's drop it from the OF code.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- drivers/of/platform.c
+++ drivers/of/platform.c
@@ -190,7 +190,6 @@ static struct platform_device *of_platform_device_create_pdata(
 		dev->dev.dma_mask = &dev->dev.coherent_dma_mask;
 	dev->dev.bus = &platform_bus_type;
 	dev->dev.platform_data = platform_data;
-	of_msi_configure(&dev->dev, dev->dev.of_node);
 
 	if (of_device_add(dev) != 0) {
 		platform_device_put(dev);

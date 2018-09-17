soccertack/nesting-linaro

KVM: arm64: Emulate TLBI VMALLS12E1(IS) instruction
Based on the same principle as TLBI ALLE1(IS) emulation, we clear the
mappings in the shadow stage-2 page tables and invalidate TLB entries.
But this time we do it only for the current VMID from the guest
hypervisor's perspective, not for all VMIDs.

Signed-off-by: Jintack Lim <jintack.lim@linaro.org>

arch/arm64/kvm/mmu-nested.c
@@ -348,6 +348,29 @@ static struct kvm_nested_s2_mmu *lookup_nested_mmu(struct kvm_vcpu *vcpu,
 	return NULL;
 }
 
+/*
+ * Clear mappings in the shadow stage 2 page tables for the current VMID from
+ * the perspective of the guest hypervisor.
+ * This function expects kvm->mmu_lock to be held.
+ */
+bool kvm_nested_s2_clear_curr_vmid(struct kvm_vcpu *vcpu, phys_addr_t start,
+				   u64 size)
+{
+	struct kvm_nested_s2_mmu *nested_mmu;
+	u64 vttbr = vcpu_sys_reg(vcpu, VTTBR_EL2);
+
+	/*
+	 * Look up a mmu that is used for the current VMID from the guest
+	 * hypervisor's view.
+	 */
+	nested_mmu = lookup_nested_mmu(vcpu, vttbr);
+	if (!nested_mmu)
+		return false;
+
+	kvm_unmap_stage2_range(vcpu->kvm, &nested_mmu->mmu, start, size);
+	return true;
+}
+
 /**
  * create_nested_mmu - create mmu for the given virtual VMID
  *

kernelorg-mirror/linux_kernel_git_maz_arm-platforms

arch/arm64/kvm/mmu-nested.c
@@ -349,6 +349,29 @@ static struct kvm_nested_s2_mmu *lookup_nested_mmu(struct kvm_vcpu *vcpu,
 	return NULL;
 }
 
+/*
+ * Clear mappings in the shadow stage 2 page tables for the current VMID from
+ * the perspective of the guest hypervisor.
+ * This function expects kvm->mmu_lock to be held.
+ */
+bool kvm_nested_s2_clear_curr_vmid(struct kvm_vcpu *vcpu, phys_addr_t start,
+				   u64 size)
+{
+	struct kvm_nested_s2_mmu *nested_mmu;
+	u64 vttbr = __vcpu_sys_reg(vcpu, VTTBR_EL2);
+
+	/*
+	 * Look up a mmu that is used for the current VMID from the guest
+	 * hypervisor's view.
+	 */
+	nested_mmu = lookup_nested_mmu(vcpu, vttbr);
+	if (!nested_mmu)
+		return false;
+
+	kvm_unmap_stage2_range(vcpu->kvm, &nested_mmu->mmu, start, size);
+	return true;
+}
+
 /**
  * create_nested_mmu - create mmu for the given virtual VMID
  *
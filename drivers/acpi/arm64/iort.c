3ad6511c92c754795e37d07232131c496af43132
Marc Zyngier <marc.zyngier@arm.com>
2018-11-19 14:37:26 +0000

    WIP
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- drivers/acpi/arm64/iort.c
+++ drivers/acpi/arm64/iort.c
@@ -222,32 +222,17 @@ IRQCHIP_ACPI_DECLARE(iort_its, ACPI_MADT_TYPE_GENERIC_DISTRIBUTOR,
 struct fwnode_handle *iort_get_domain_token(int trans_id, phys_addr_t base)
 {
 	struct iort_its_msi_chip *its_msi_chip;
-	struct fwnode_handle *fw_node;
+	struct fwnode_handle *fw_node = NULL;
 
 	mutex_lock(&iort_msi_chip_lock);
 
+	if (list_empty(&iort_msi_chip_list))
+		pr_warn("ITS list not populated\n");
+
 	its_msi_chip = __get_msi_chip(trans_id);
-	if (its_msi_chip) {
+	if (its_msi_chip)
 		fw_node = its_msi_chip->fw_node;
-		goto out;
-	}
 
-	fw_node = irq_domain_alloc_fwnode((void *)(uintptr_t)trans_id);
-	its_msi_chip = kzalloc(sizeof(*its_msi_chip), GFP_KERNEL);
-	if (!its_msi_chip || !fw_node) {
-		kfree(its_msi_chip);
-		irq_domain_free_fwnode(fw_node);
-		fw_node = NULL;
-		goto out;
-	}
-
-	its_msi_chip->fw_node = fw_node;
-	its_msi_chip->translation_id = trans_id;
-	its_msi_chip->base_addr = base;
-
-	list_add(&its_msi_chip->list, &iort_msi_chip_list);
-
-out:
 	mutex_unlock(&iort_msi_chip_lock);
 
 	return fw_node;

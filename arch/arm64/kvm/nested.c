dc1fdf583fe886350c842e461aedb7c15c8f4520
Marc Zyngier <maz@kernel.org>
2018-12-25 12:20:56 +0000

    arm64: KVM: nv: Drop vcpu from check_output_size
    check_output_size() never uses the vcpu, so let's drop it.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/nested.c
+++ arch/arm64/kvm/nested.c
@@ -152,8 +152,7 @@ static int check_base_s2_limits(struct kvm_vcpu *vcpu, struct s2_walk_info *wi,
 }
 
 /* Check if output is within boundaries */
-static int check_output_size(struct kvm_vcpu *vcpu, struct s2_walk_info *wi,
-			     phys_addr_t output)
+static int check_output_size(struct s2_walk_info *wi, phys_addr_t output)
 {
 	unsigned int output_size = ps_to_output_size(wi->ps);
 
@@ -211,7 +210,7 @@ static int walk_nested_s2_pgd(struct kvm_vcpu *vcpu, phys_addr_t ipa,
 			   wi->pgshift);
 	base_addr = wi->baddr & GENMASK_ULL(47, base_lower_bound);
 
-	if (check_output_size(vcpu, wi, base_addr)) {
+	if (check_output_size(wi, base_addr)) {
 		out->esr = compute_fsc(level, ESR_ELx_FSC_ADDRSZ);
 		return 1;
 	}
@@ -249,7 +248,7 @@ static int walk_nested_s2_pgd(struct kvm_vcpu *vcpu, phys_addr_t ipa,
 		if ((desc & 3) == 1 || level == 3)
 			break;
 
-		if (check_output_size(vcpu, wi, desc)) {
+		if (check_output_size(wi, desc)) {
 			out->esr = compute_fsc(level, ESR_ELx_FSC_ADDRSZ);
 			return 1;
 		}
@@ -270,7 +269,7 @@ static int walk_nested_s2_pgd(struct kvm_vcpu *vcpu, phys_addr_t ipa,
 	 * for misprogramming of the contiguous bit.
 	 */
 
-	if (check_output_size(vcpu, wi, desc)) {
+	if (check_output_size(wi, desc)) {
 		out->esr = compute_fsc(level, ESR_ELx_FSC_ADDRSZ);
 		return 1;
 	}

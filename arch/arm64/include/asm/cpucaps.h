5bb2bec5c3fbdde9568604c586bd660294b8be7c
Marc Zyngier <maz@kernel.org>
2019-01-23 19:12:17 +0100

    KVM: arm64: Add ARMv8.4 Enhanced Nested Virt cpufeature
    Add the detection code for the ARMv8.4-NV feature.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/cpucaps.h
+++ arch/arm64/include/asm/cpucaps.h
@@ -63,7 +63,8 @@
 #define ARM64_HAS_GENERIC_AUTH			53
 #define ARM64_HAS_ARMv8_4_TTL			54
 #define ARM64_HAS_NESTED_VIRT			55
+#define ARM64_HAS_ENHANCED_NESTED_VIRT		56
 
-#define ARM64_NCAPS				56
+#define ARM64_NCAPS				57
 
 #endif /* __ASM_CPUCAPS_H */

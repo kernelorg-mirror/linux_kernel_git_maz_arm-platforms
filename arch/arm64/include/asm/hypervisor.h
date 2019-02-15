245dccfef44df2cd4d9b3612b000564ee0147f81
Will Deacon <will.deacon@arm.com>
2019-02-15 11:49:27 +0000

    arm64: Define pv_cond_yield_t data structure
    ...
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/include/asm/hypervisor.h
+++ arch/arm64/include/asm/hypervisor.h
@@ -2,6 +2,16 @@
 #ifndef _ASM_ARM64_HYPERVISOR_H
 #define _ASM_ARM64_HYPERVISOR_H
 
+#include <linux/types.h>
 #include <asm/xen/hypervisor.h>
+
+typedef struct pv_cond_yield_ent {
+	u64	pc;
+	u64	mask;
+	u8	val_reg;
+	u8	addr_reg;
+	u16	__reserved;
+	u32	flags;
+} pv_cond_yield_ent_t;
 
 #endif

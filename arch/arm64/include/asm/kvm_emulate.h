61fae8db146a893fb218678962519d4a0c4d1e0c
Marc Zyngier <marc.zyngier@arm.com>
2019-02-15 16:10:07 +0000

    fixup! KVM: arm64: nv: Handle shadow stage 2 page faults

--- arch/arm64/include/asm/kvm_emulate.h
+++ arch/arm64/include/asm/kvm_emulate.h
@@ -592,7 +592,8 @@ static inline void __hyp_text __kvm_skip_instr(struct kvm_vcpu *vcpu)
 
 static inline bool kvm_is_shadow_s2_fault(struct kvm_vcpu *vcpu)
 {
-	return vcpu->arch.hw_mmu != &vcpu->kvm->arch.mmu;
+	return (vcpu->arch.hw_mmu != &vcpu->kvm->arch.mmu &&
+		vcpu->arch.hw_mmu->nested_stage2_enabled);
 }
 
 #endif /* __ARM64_KVM_EMULATE_H__ */

6eb459b1363a34dc243c2bba730df22ea0c35e9c
Marc Zyngier <marc.zyngier@arm.com>
2019-06-28 23:43:58 +0100

    VNCR-ize SPSR_EL1
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -185,6 +185,7 @@ enum vcpu_sysreg {
 	DISR_EL1,	/* Deferred Interrupt Status Register */
 	VNCR(ELR_EL1),
 	VNCR(SP_EL1),
+	VNCR(SPSR_EL1),
 
 	/* Performance Monitors Registers */
 	PMCR_EL0,	/* Control Register */
@@ -308,7 +309,6 @@ enum vcpu_sysreg {
 struct kvm_cpu_context {
 	struct user_pt_regs regs;	/* sp = sp_el0 */
 
-	u64	spsr_el1;		/* aka spsr_svc */
 	u64	spsr_abt;
 	u64	spsr_und;
 	u64	spsr_irq;

f591a85fbfc991db873e44c7456e5fa786c6644b
Marc Zyngier <marc.zyngier@arm.com>
2019-07-08 11:54:01 +0100

    fixup! HACK: clk-bcm2835: Add BCM2838_CLOCK_EMMC2 support

--- drivers/clk/bcm/clk-bcm2835.c
+++ drivers/clk/bcm/clk-bcm2835.c
@@ -2179,10 +2179,7 @@ static int bcm2835_clk_probe(struct platform_device *pdev)
 	for (i = 0; i < asize; i++) {
 		desc = &clk_desc_array[i];
 		if (desc->clk_register && desc->data) {
-			if ((i != BCM2838_CLOCK_EMMC2) ||
-			    of_device_is_compatible(fw_node, "brcm,bcm2838-cprman")) {
-				hws[i] = desc->clk_register(cprman, desc->data);
-			}
+			hws[i] = desc->clk_register(cprman, desc->data);
 		}
 	}
 

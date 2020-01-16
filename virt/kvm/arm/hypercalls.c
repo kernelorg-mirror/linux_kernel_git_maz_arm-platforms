c6b15e1ce9af00da09185fa777d2754a3ca1ab3d
Marc Zyngier <maz@kernel.org>
2020-01-16 14:18:03 +0000

    rebase fixes
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- virt/kvm/arm/hypercalls.c
+++ virt/kvm/arm/hypercalls.c
@@ -12,13 +12,15 @@
 int kvm_hvc_call_handler(struct kvm_vcpu *vcpu)
 {
 	u32 func_id = smccc_get_function(vcpu);
-	long val = SMCCC_RET_NOT_SUPPORTED;
+	long val[4] = {
+		[0] = SMCCC_RET_NOT_SUPPORTED,
+	};
 	u32 feature;
 	gpa_t gpa;
 
 	switch (func_id) {
 	case ARM_SMCCC_VERSION_FUNC_ID:
-		val = ARM_SMCCC_VERSION_1_1;
+		val[0] = ARM_SMCCC_VERSION_1_1;
 		break;
 	case ARM_SMCCC_ARCH_FEATURES_FUNC_ID:
 		feature = smccc_get_arg1(vcpu);
@@ -28,10 +30,10 @@ int kvm_hvc_call_handler(struct kvm_vcpu *vcpu)
 			case KVM_BP_HARDEN_UNKNOWN:
 				break;
 			case KVM_BP_HARDEN_WA_NEEDED:
-				val = SMCCC_RET_SUCCESS;
+				val[0] = SMCCC_RET_SUCCESS;
 				break;
 			case KVM_BP_HARDEN_NOT_REQUIRED:
-				val = SMCCC_RET_NOT_REQUIRED;
+				val[0] = SMCCC_RET_NOT_REQUIRED;
 				break;
 			}
 			break;
@@ -41,26 +43,26 @@ int kvm_hvc_call_handler(struct kvm_vcpu *vcpu)
 			case KVM_SSBD_UNKNOWN:
 				break;
 			case KVM_SSBD_KERNEL:
-				val = SMCCC_RET_SUCCESS;
+				val[0] = SMCCC_RET_SUCCESS;
 				break;
 			case KVM_SSBD_FORCE_ENABLE:
 			case KVM_SSBD_MITIGATED:
-				val = SMCCC_RET_NOT_REQUIRED;
+				val[0] = SMCCC_RET_NOT_REQUIRED;
 				break;
 			}
 			break;
 		case ARM_SMCCC_HV_PV_TIME_FEATURES:
-			val = SMCCC_RET_SUCCESS;
+			val[0] = SMCCC_RET_SUCCESS;
 			break;
 		}
 		break;
 	case ARM_SMCCC_HV_PV_TIME_FEATURES:
-		val = kvm_hypercall_pv_features(vcpu);
+		val[0] = kvm_hypercall_pv_features(vcpu);
 		break;
 	case ARM_SMCCC_HV_PV_TIME_ST:
 		gpa = kvm_init_stolen_time(vcpu);
 		if (gpa != GPA_INVALID)
-			val = gpa;
+			val[0] = gpa;
 		break;
 	case ARM_SMCCC_VENDOR_HYP_CALL_UID_FUNC_ID:
 		val[0] = ARM_SMCCC_VENDOR_HYP_UID_KVM_REG_0;
@@ -74,12 +76,9 @@ int kvm_hvc_call_handler(struct kvm_vcpu *vcpu)
 			 val[0] |= BIT(ARM_SMCCC_KVM_FUNC_PV_COND_YIELD);
 		break;
 	case ARM_SMCCC_VENDOR_HYP_KVM_PV_COND_YIELD_FUNC_ID:
-		if (vcpu_el1_is_32bit(vcpu) || !has_vhe()) {
-			val[0] = SMCCC_RET_NOT_SUPPORTED;
+		if (vcpu_el1_is_32bit(vcpu) || !has_vhe())
 			break;
-		}
-		option = smccc_get_arg1(vcpu);
-		switch (option) {
+		switch (smccc_get_arg1(vcpu)) {
 		case KVM_PV_COND_YIELD_OP_REGISTER:
 			kvm_pvcy_populate(vcpu->kvm,
 					  smccc_get_arg2(vcpu),
@@ -98,6 +97,6 @@ int kvm_hvc_call_handler(struct kvm_vcpu *vcpu)
 		return kvm_psci_call(vcpu);
 	}
 
-	smccc_set_retval(vcpu, val, 0, 0, 0);
+	smccc_set_retval(vcpu, val[0], val[1], val[2], val[3]);
 	return 1;
 }

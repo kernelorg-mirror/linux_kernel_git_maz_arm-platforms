50f088aa6bd27bbe79ab9cf238b35fbb1f2ce80c
Marc Zyngier <maz@kernel.org>
2020-01-22 12:53:47 +0000

    KVM: arm64: nv: Fast-track EL1 TLBIs for VHE guests
    Due to the way ARMv8.4-NV suppresses traps when accessing EL2
    system registers, we can't track when the guest changes its
    HCR_EL2.TGE setting. This means we always trap EL1 TLBIs,
    even if they don't affect any guest.
    
    This obviously has a huge impact on performance, as we handle
    TLBI traps as a normal exit, and a normal VHE host issues
    thousands of TLBIs when booting (and quite a few when running
    userspace).
    
    A cheap way to reduce the overhead is to handle the limited
    case of {E2H,TGE}=={1,1} as a guest fixup, as we already have
    the right mmu configuration in place. Just execute the decoded
    instruction right away and return to the guest.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/hyp/tlb.c
+++ arch/arm64/kvm/hyp/tlb.c
@@ -276,7 +276,8 @@ void __hyp_text __kvm_tlb_el1_instr(struct kvm_s2_mmu *mmu, u64 val, u64 sys_enc
 	dsb(ishst);
 
 	/* Switch to requested VMID */
-	__tlb_switch_to_guest(mmu, &cxt);
+	if (mmu)
+		__tlb_switch_to_guest(mmu, &cxt);
 
 	/*
 	 * Execute the same instruction as the guest hypervisor did,
@@ -315,5 +316,6 @@ void __hyp_text __kvm_tlb_el1_instr(struct kvm_s2_mmu *mmu, u64 val, u64 sys_enc
 	dsb(ish);
 	isb();
 
-	__tlb_switch_to_host(&cxt);
+	if (mmu)
+		__tlb_switch_to_host(&cxt);
 }

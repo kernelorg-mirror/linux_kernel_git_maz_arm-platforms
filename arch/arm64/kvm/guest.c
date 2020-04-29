75125651bcce34e693f46b8fe1d640b2a0b3b352
Marc Zyngier <maz@kernel.org>
2020-04-29 11:21:55 +0100

    KVM: arm64: Fix 32bit PC wrap-around
    In the unlikely event that a 32bit vcpu traps into the hypervisor
    on an instruction that is located right at the end of the 32bit
    range, the emulation of that instruction is going to increment
    PC past the 32bit range. This isn't great, as userspace can then
    observe this value and get a bit confused.
    
    Conversly, userspace can do things like (in the context of a 64bit
    guest that is capable of 32bit EL0) setting PSTATE to AArch64-EL0,
    set PC to a 64bit value, change PSTATE to AArch32-USR, and observe
    that PC hasn't been truncated. More confusion.
    
    Fix both by:
    - truncating PC increments for 32bit guests
    - sanitize PC every time a core reg is changed by userspace, and
      that PSTATE indicates a 32bit mode.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/guest.c
+++ arch/arm64/kvm/guest.c
@@ -200,6 +200,10 @@ static int set_core_reg(struct kvm_vcpu *vcpu, const struct kvm_one_reg *reg)
 	}
 
 	memcpy((u32 *)regs + off, valp, KVM_REG_SIZE(reg->id));
+
+	if (*vcpu_cpsr(vcpu) & PSR_AA32_MODE_MASK)
+		*vcpu_pc(vcpu) = lower_32_bits(*vcpu_pc(vcpu));
+
 out:
 	return err;
 }

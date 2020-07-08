a102422ae0ff0a47602e6db4337456567ea5c4fa
Marc Zyngier <maz@kernel.org>
2020-07-08 20:07:23 +0100

    KVM: arm64: nv: Save/Restore vEL2 sysregs
    Whenever we need to restore the guest's system registers to the CPU, we
    now need to take care of the EL2 system registers as well. Most of them
    are accessed via traps only, but some have an immediate effect and also
    a guest running in VHE mode would expect them to be accessible via their
    EL1 encoding, which we do not trap.
    
    For vEL2 we write the virtual EL2 registers with an identical format directly
    into their EL1 counterpart, and translate the few registers that have a
    different format for the same effect on the execution when running a
    non-VHE guest guest hypervisor.
    
    Based on an initial patch from Andre Przywara, rewritten many times
    since.
    
    Reviewed-by: Alexandru Elisei <alexandru.elisei@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/hyp/include/hyp/sysreg-sr.h
+++ arch/arm64/kvm/hyp/include/hyp/sysreg-sr.h
@@ -145,9 +145,10 @@ static inline void __sysreg_restore_user_state(struct kvm_cpu_context *ctxt)
 		write_sysreg_s(ctxt_sys_reg(ctxt, ACCDATA_EL1), SYS_ACCDATA_EL1);
 }
 
-static inline void __sysreg_restore_el1_state(struct kvm_cpu_context *ctxt)
+static inline void __sysreg_restore_el1_state(struct kvm_cpu_context *ctxt,
+					      u64 mpidr)
 {
-	write_sysreg(ctxt_sys_reg(ctxt, MPIDR_EL1),	vmpidr_el2);
+	write_sysreg(mpidr,				vmpidr_el2);
 
 	if (has_vhe() ||
 	    !cpus_have_final_cap(ARM64_WORKAROUND_SPECULATIVE_AT)) {

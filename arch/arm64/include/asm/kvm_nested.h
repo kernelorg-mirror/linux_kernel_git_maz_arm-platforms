c806a7aa43875378f9d2495742f1e5ee7c0369e3
Marc Zyngier <maz@kernel.org>
2020-09-08 16:52:07 +0100

    KVM: arm64: nv: Tag shadow S2 entries with guest's leaf S2 level
    Populate bits [56:55] of the leaf entry with the level provided
    by the guest's S2 translation. This will allow us to better scope
    the invalidation by remembering the mapping size.
    
    Of course, this assume that the guest will issue an invalidation
    with an address that falls into the same leaf. If the guest doesn't,
    we'll over-invalidate.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/kvm_nested.h
+++ arch/arm64/include/asm/kvm_nested.h
@@ -5,6 +5,7 @@
 #include <linux/bitfield.h>
 #include <linux/kvm_host.h>
 #include <asm/kvm_emulate.h>
+#include <asm/kvm_pgtable.h>
 
 static inline bool vcpu_has_nv(const struct kvm_vcpu *vcpu)
 {
@@ -181,5 +182,12 @@ static inline bool kvm_supported_tlbi_s1e2_op(struct kvm_vcpu *vpcu, u32 instr)
 }
 
 int kvm_init_nv_sysregs(struct kvm *kvm);
+
+#define KVM_NV_GUEST_MAP_SZ	(KVM_PGTABLE_PROT_SW1 | KVM_PGTABLE_PROT_SW0)
+
+static inline u64 kvm_encode_nested_level(struct kvm_s2_trans *trans)
+{
+	return FIELD_PREP(KVM_NV_GUEST_MAP_SZ, trans->level);
+}
 
 #endif /* __ARM64_KVM_NESTED_H */

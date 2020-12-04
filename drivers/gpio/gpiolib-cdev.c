ef86034735fe5620d151e9544557ccf1116b4fef
Marc Zyngier <maz@kernel.org>
2020-12-04 14:34:52 +0000

    gpiolib: cdev: Flag invalid GPIOs as used
    When reporting the state of a GPIO to userspace, we never check
    for the actual validity of the line, meaning we report invalid
    lines as being usable. A subsequent request will fail though,
    which is an inconsistent behaviour from a userspace perspective.
    
    Instead, let's check for the validity of the line and report it
    as used if it is invalid. This allows a tool such as gpioinfo
    to report something sensible:
    
    gpiochip3 - 4 lines:
    	line   0:      unnamed       unused   input  active-high
    	line   1:      unnamed       kernel   input  active-high [used]
    	line   2:      unnamed       kernel   input  active-high [used]
    	line   3:      unnamed       unused   input  active-high
    
    In this example, lines 1 and 2 are invalid, and cannot be used by
    userspace.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- drivers/gpio/gpiolib-cdev.c
+++ drivers/gpio/gpiolib-cdev.c
@@ -1910,6 +1910,7 @@ static void gpio_desc_to_lineinfo(struct gpio_desc *desc,
 	    test_bit(FLAG_USED_AS_IRQ, &desc->flags) ||
 	    test_bit(FLAG_EXPORT, &desc->flags) ||
 	    test_bit(FLAG_SYSFS, &desc->flags) ||
+	    !gpiochip_line_is_valid(gc, info->offset) ||
 	    !ok_for_pinctrl)
 		info->flags |= GPIO_V2_LINE_FLAG_USED;
 

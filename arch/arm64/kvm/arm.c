28488c2705a729dd93423ad99f83ca8a983ce3ad
Marc Zyngier <maz@kernel.org>
2021-10-14 12:18:48 +0100

    KVM: arm64: Restructure the point where has_run_once is advertised
    Restructure kvm_arch_vcpu_run_pid_change() to set the has_run_once
    flag after having completed all the "run once" activities.
    
    This includes moving the flip of the userspace irqchip static key
    to a point where nothing can fail (the current code could end-up
    in a bizarre state in a few error cases).
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -597,8 +597,6 @@ int kvm_arch_vcpu_run_pid_change(struct kvm_vcpu *vcpu)
 	if (likely(vcpu->arch.has_run_once))
 		return 0;
 
-	vcpu->arch.has_run_once = true;
-
 	kvm_arm_vcpu_init_debug(vcpu);
 
 	if (likely(irqchip_in_kernel(kvm))) {
@@ -609,12 +607,6 @@ int kvm_arch_vcpu_run_pid_change(struct kvm_vcpu *vcpu)
 		ret = kvm_vgic_map_resources(kvm);
 		if (ret)
 			return ret;
-	} else {
-		/*
-		 * Tell the rest of the code that there are userspace irqchip
-		 * VMs in the wild.
-		 */
-		static_branch_inc(&userspace_irqchip_in_use);
 	}
 
 	ret = kvm_timer_enable(vcpu);
@@ -622,6 +614,18 @@ int kvm_arch_vcpu_run_pid_change(struct kvm_vcpu *vcpu)
 		return ret;
 
 	ret = kvm_arm_pmu_v3_enable(vcpu);
+	if (ret)
+		return ret;
+
+	if (!irqchip_in_kernel(kvm)) {
+		/*
+		 * Tell the rest of the code that there are userspace irqchip
+		 * VMs in the wild.
+		 */
+		static_branch_inc(&userspace_irqchip_in_use);
+	}
+
+	vcpu->arch.has_run_once = true;
 
 	/*
 	 * Initialize traps for protected VMs.

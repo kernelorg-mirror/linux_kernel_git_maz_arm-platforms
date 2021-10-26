2cab04d7860f3c2e5700f9ba1f7a39a4a156c5f5
Quentin Perret <qperret@google.com>
2021-10-26 10:06:36 +0100

    KVM: arm64: Make owner ids 32 bits wide
    We will soon need more than 8 bits to encode all possible owner ids in
    KVM protected mode. To prepare the ground for this, introduce a new type
    for owner_ids, and make it a 32bits wide.
    
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/hyp/nvhe/mem_protect.c
+++ arch/arm64/kvm/hyp/nvhe/mem_protect.c
@@ -27,8 +27,9 @@ struct host_kvm host_kvm;
 
 static struct hyp_pool host_s2_pool;
 
-const u8 pkvm_host_id = 0;
-const u8 pkvm_hyp_id = 1;
+const pkvm_id pkvm_host_id	= 0;
+const pkvm_id pkvm_hyp_id	= 1;
+const pkvm_id pkvm_guest_id	= 2;
 
 static DEFINE_PER_CPU(struct kvm_shadow_vm *, __current_vm);
 #define current_vm (*this_cpu_ptr(&__current_vm))
@@ -512,14 +513,14 @@ int host_stage2_idmap_locked(phys_addr_t addr, u64 size,
 	return host_stage2_try(__host_stage2_idmap, addr, addr + size, prot);
 }
 
-#define KVM_INVALID_PTE_OWNER_MASK	GENMASK(9, 2)
-static kvm_pte_t kvm_init_invalid_leaf_owner(u8 owner_id)
+#define KVM_INVALID_PTE_OWNER_MASK	GENMASK(32, 1)
+static kvm_pte_t kvm_init_invalid_leaf_owner(pkvm_id owner_id)
 {
 	return FIELD_PREP(KVM_INVALID_PTE_OWNER_MASK, owner_id);
 }
 
 int host_stage2_set_owner_locked(phys_addr_t addr, u64 size,
-				 u8 owner_id)
+				 pkvm_id owner_id)
 {
 	kvm_pte_t annotation = kvm_init_invalid_leaf_owner(owner_id);
 
@@ -631,7 +632,7 @@ struct pkvm_mem_donation {
 	const struct pkvm_mem_transition	tx;
 };
 
-static u8 completer_owner_id(const struct pkvm_mem_transition *tx)
+static pkvm_id completer_owner_id(const struct pkvm_mem_transition *tx)
 {
 	switch (tx->completer.id) {
 	case PKVM_ID_HOST:
@@ -746,7 +747,7 @@ static int host_initiate_unshare(u64 *completer_addr,
 static int host_initiate_donation(u64 *completer_addr,
 				  const struct pkvm_mem_transition *tx)
 {
-	u8 owner_id = completer_owner_id(tx);
+	pkvm_id owner_id = completer_owner_id(tx);
 	u64 size = tx->nr_pages * PAGE_SIZE;
 
 	*completer_addr = tx->initiator.host.completer_addr;
@@ -772,7 +773,7 @@ static int host_ack_donation(u64 addr, const struct pkvm_mem_transition *tx)
 static int host_complete_donation(u64 addr, const struct pkvm_mem_transition *tx)
 {
 	u64 size = tx->nr_pages * PAGE_SIZE;
-	u8 host_id = completer_owner_id(tx);
+	pkvm_id host_id = completer_owner_id(tx);
 
 	return host_stage2_set_owner_locked(addr, size, host_id);
 }

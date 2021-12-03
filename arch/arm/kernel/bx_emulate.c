shannonz88/arm-platforms

ARM: Allow dynamic switching between emulation and patching of BX
In order to measure the impact of emulation vs patching, allow
the mode to be controlled from userspace:

root@debian:~# cat /proc/cpu/bx_emulation
Patched BX:	0
Emulated BX:	8685162
root@debian:~# echo P > /proc/cpu/bx_emulation
root@debian:~# cat /proc/cpu/bx_emulation
Patched BX:	453
Emulated BX:	8691516
root@debian:~# echo E > /proc/cpu/bx_emulation
root@debian:~# cat /proc/cpu/bx_emulation
Patched BX:	551
Emulated BX:	8692193

The kernel defaults to emulating the BX instruction.

Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>

arch/arm/kernel/bx_emulate.c
@@ -22,6 +22,7 @@
 
 #include <asm/cacheflush.h>
 #include <asm/traps.h>
+#include <asm/uaccess.h>
 
 static unsigned long bx_emul_counter;
 static unsigned long bx_patch_counter;
@@ -41,9 +42,24 @@ static int bx_proc_open(struct inode *inode, struct file *file)
 	return single_open(file, bx_proc_show, PDE_DATA(inode));
 }
 
+static ssize_t bx_proc_write(struct file *file, const char __user *buf,
+			     size_t count, loff_t *ppos)
+{
+	char c;
+
+	if (!get_user(c, buf)) {
+		if (c == 'P')
+			bx_patch_mode = 1;
+		if (c == 'E')
+			bx_patch_mode = 0;
+	}
+	return count;
+}
+
 static const struct file_operations bx_proc_fops = {
 	.open		= bx_proc_open,
 	.read		= seq_read,
+	.write		= bx_proc_write,
 	.llseek		= seq_lseek,
 	.release	= single_release,
 };

kernelorg-mirror/linux_kernel_git_maz_arm-platforms

arch/arm/kernel/bx_emulate.c
@@ -22,6 +22,7 @@
 
 #include <asm/cacheflush.h>
 #include <asm/traps.h>
+#include <asm/uaccess.h>
 
 static unsigned long bx_emul_counter;
 static unsigned long bx_patch_counter;
@@ -41,9 +42,24 @@ static int bx_proc_open(struct inode *inode, struct file *file)
 	return single_open(file, bx_proc_show, PDE_DATA(inode));
 }
 
+static ssize_t bx_proc_write(struct file *file, const char __user *buf,
+			     size_t count, loff_t *ppos)
+{
+	char c;
+
+	if (!get_user(c, buf)) {
+		if (c == 'P')
+			bx_patch_mode = 1;
+		if (c == 'E')
+			bx_patch_mode = 0;
+	}
+	return count;
+}
+
 static const struct proc_ops bx_proc_ops = {
 	.proc_open	= bx_proc_open,
 	.proc_read	= seq_read,
+	.proc_write	= bx_proc_write,
 	.proc_lseek	= seq_lseek,
 	.proc_release	= single_release,
 };
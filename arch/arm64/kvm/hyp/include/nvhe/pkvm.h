323c09675585f5a9d175a01e8770f5346e6a4afd
Fuad Tabba <tabba@google.com>
2022-02-23 11:01:59 +0000

    KVM: arm64: get_shadow_vcpu()'s vcpu_idx should be unsigned
    fixup! KVM: arm64: Refcount shadow structs on vcpu_{load/put}()
    
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/hyp/include/nvhe/pkvm.h
+++ arch/arm64/kvm/hyp/include/nvhe/pkvm.h
@@ -60,7 +60,7 @@ static inline bool vcpu_is_protected(struct kvm_vcpu *vcpu)
 void hyp_shadow_table_init(void *tbl);
 int __pkvm_init_shadow(struct kvm *kvm, void *shadow_va, size_t size, void *pgd);
 int __pkvm_teardown_shadow(int shadow_handle);
-struct kvm_vcpu *get_shadow_vcpu(int shadow_handle, int vcpu_idx);
+struct kvm_vcpu *get_shadow_vcpu(int shadow_handle, unsigned int vcpu_idx);
 void put_shadow_vcpu(struct kvm_vcpu *vcpu);
 
 u64 pvm_read_id_reg(const struct kvm_vcpu *vcpu, u32 id);

c2c3df91a76de32954bfbd255860456a49cfcebf
Will Deacon <will@kernel.org>
2022-04-12 15:45:11 +0100

    KVM: arm64: Split up nvhe/fixed_config.h
    In preparation for using some of the pKVM fixed configuration register
    definitions to filter the available VM CAPs in the host, split the
    nvhe/fixed_config.h header so that the definitions can be shared
    with the host, while keeping the hypervisor function prototypes in
    the nvhe/ namespace.
    
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/kvm/hyp/include/nvhe/pkvm.h
+++ arch/arm64/kvm/hyp/include/nvhe/pkvm.h
@@ -66,5 +66,9 @@ struct kvm_shadow_vcpu_state *
 pkvm_load_shadow_vcpu_state(unsigned int shadow_handle, unsigned int vcpu_idx);
 void pkvm_put_shadow_vcpu_state(struct kvm_shadow_vcpu_state *shadow_state);
 
-#endif /* __ARM64_KVM_NVHE_PKVM_H__ */
+u64 pvm_read_id_reg(const struct kvm_vcpu *vcpu, u32 id);
+bool kvm_handle_pvm_sysreg(struct kvm_vcpu *vcpu, u64 *exit_code);
+bool kvm_handle_pvm_restricted(struct kvm_vcpu *vcpu, u64 *exit_code);
+int kvm_check_pvm_sysreg_table(void);
 
+#endif /* __ARM64_KVM_NVHE_PKVM_H__ */

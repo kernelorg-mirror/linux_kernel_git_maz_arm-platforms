b4c0f13478ae1d695e53358f9b88a38a1ecbb732
Marc Zyngier <maz@kernel.org>
2022-05-25 17:03:49 +0100

    KVM: arm64: Move vcpu PC/Exception flags to the input flag set
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/kvm_emulate.h
+++ arch/arm64/include/asm/kvm_emulate.h
@@ -473,7 +473,7 @@ static inline unsigned long vcpu_data_host_to_guest(struct kvm_vcpu *vcpu,
 
 static __always_inline void kvm_incr_pc(struct kvm_vcpu *vcpu)
 {
-	vcpu->arch.flags |= KVM_ARM64_INCREMENT_PC;
+	vcpu_set_flag(vcpu, INCREMENT_PC);
 }
 
 static inline bool vcpu_has_feature(struct kvm_vcpu *vcpu, int feature)

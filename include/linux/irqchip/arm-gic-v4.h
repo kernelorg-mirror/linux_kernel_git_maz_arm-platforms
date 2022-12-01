25f2d8db395c86c637bee12a4982c42c6a2f137d
Marc Zyngier <maz@kernel.org>
2022-12-01 11:49:05 +0000

    KVM: arm64: vgic-v4: Propagate the NMI state into the GICv4.1 VSGI configuration
    Just as we now allow the NMI state to make it into the LRs,
    allow the same state to be propagated into the VSGI configuration.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- include/linux/irqchip/arm-gic-v4.h
+++ include/linux/irqchip/arm-gic-v4.h
@@ -57,6 +57,7 @@ struct its_vpe {
 				u8	priority;
 				bool	enabled;
 				bool	group;
+				bool	nmi;
 			}			sgi_config[16];
 			atomic_t vmapp_count;
 		};
@@ -124,6 +125,7 @@ struct its_cmd_info {
 		struct {
 			u8		priority;
 			bool		group;
+			bool		nmi;
 		};
 	};
 };
@@ -138,7 +140,7 @@ int its_map_vlpi(int irq, struct its_vlpi_map *map);
 int its_get_vlpi(int irq, struct its_vlpi_map *map);
 int its_unmap_vlpi(int irq);
 int its_prop_update_vlpi(int irq, u8 config, bool inv);
-int its_prop_update_vsgi(int irq, u8 priority, bool group);
+int its_prop_update_vsgi(int irq, u8 priority, bool group, bool nmi);
 
 struct irq_domain_ops;
 int its_init_v4(struct irq_domain *domain,

64d76f3fa13f1f8d52a85272729135bbce6f660c
Marc Zyngier <maz@kernel.org>
2023-05-26 12:16:05 +0100

    KVM: arm64: Opportunistically track HCR_EL2.E2H being flipped
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -559,6 +559,9 @@ struct kvm_vcpu_arch {
 	/* State flags for kernel bookkeeping, unused by the hypervisor code */
 	u8 sflags;
 
+	/* Output flags from the hypervisor to the kernel */
+	u8 oflags;
+
 	/*
 	 * Don't run the guest (internal implementation need).
 	 *
@@ -780,6 +783,8 @@ struct kvm_vcpu_arch {
 /* Software step state is Active-pending */
 #define DBG_SS_ACTIVE_PENDING	__vcpu_single_flag(sflags, BIT(5))
 
+/* vcpu entered with HCR_EL2.E2H set */
+#define VCPU_HCR_E2H		__vcpu_single_flag(oflags, BIT(0))
 
 /* Pointer to the vcpu's SVE FFR for sve_{save,load}_state() */
 #define vcpu_sve_pffr(vcpu) (kern_hyp_va((vcpu)->arch.sve_state) +	\

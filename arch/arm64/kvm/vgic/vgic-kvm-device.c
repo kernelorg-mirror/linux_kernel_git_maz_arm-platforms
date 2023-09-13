5d219694e4c0e6ec6e1a2168b24cde8b0a3b4f7f
Marc Zyngier <maz@kernel.org>
2023-09-13 23:05:56 +0100

    KVM: arm64: vgic-v2: Use cpuid from userspace as vcpu_id
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/vgic/vgic-kvm-device.c
+++ arch/arm64/kvm/vgic/vgic-kvm-device.c
@@ -345,7 +345,7 @@ int vgic_v2_parse_attr(struct kvm_device *dev, struct kvm_device_attr *attr,
 	if (cpuid >= atomic_read(&dev->kvm->online_vcpus))
 		return -EINVAL;
 
-	reg_attr->vcpu = kvm_get_vcpu(dev->kvm, cpuid);
+	reg_attr->vcpu = kvm_get_vcpu_by_id(dev->kvm, cpuid);
 	reg_attr->addr = attr->attr & KVM_DEV_ARM_VGIC_OFFSET_MASK;
 
 	return 0;

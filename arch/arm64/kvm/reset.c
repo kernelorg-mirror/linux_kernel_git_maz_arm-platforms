983e395956566eb432198ff92415172855f673c2
Marc Zyngier <maz@kernel.org>
2024-09-24 18:59:45 +0100

    KVM: arm64: nv: Add pseudo-TLB backing VNCR_EL2
    FEAT_NV2 introduces an interesting problem for NV, as VNCR_EL2.BADDR
    is a virtual address in the EL2&0 (or EL2, but we thankfully ignore
    this) translation regime.
    
    As we need to replicate such mapping in the real EL2, it means that
    we need to remember that there is such a translation, and that any
    TLBI affecting EL2 can possibly affect this translation.
    
    It also means that any invalidation driven by an MMU notifier must
    be able to shoot down any such mapping.
    
    All in all, we need a data structure that represents this mapping,
    and that is extremely close to a TLB. Given that we can only use
    one of those per vcpu at any given time, we only allocate one.
    
    No effort is made to keep that structure small. If we need to
    start caching multiple of them, we may want to revisit that design
    point. But for now, it is kept simple so that we can reason about it.
    
    Oh, and add a braindump of how things are supposed to work, because
    I will definitely page this out at some point. Yes, pun intended.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/reset.c
+++ arch/arm64/kvm/reset.c
@@ -158,6 +158,7 @@ void kvm_arm_vcpu_destroy(struct kvm_vcpu *vcpu)
 	if (sve_state)
 		kvm_unshare_hyp(sve_state, sve_state + vcpu_sve_state_size(vcpu));
 	kfree(sve_state);
+	kfree(vcpu->arch.vncr_tlb);
 	kfree(vcpu->arch.ccsidr);
 }
 

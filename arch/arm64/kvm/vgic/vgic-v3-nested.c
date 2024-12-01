bf49be6b9b71404728507a1386274998f74df8a1
Marc Zyngier <maz@kernel.org>
2024-12-01 15:59:33 +0000

    KVM: arm64: nv: Propagate used_lrs between L1 and L0 contexts
    We have so far made sure that L1 and L0 vgic contexts were
    totally independent. There is however one spot of bother with
    this approach, and that's in the GICv3 emulation code required by
    our fruity friends.
    
    The issue is that the emulation code needs to know how many LRs
    are in flight. And while it is easy to reach the L0 version through
    the vcpu pointer, doing so for the L1 is much more complicated,
    as these structures are private to the nested code.
    
    We could simply expose that structure and pick one or the other
    depending on the context, but this seems extra complexity for not
    much benefit.
    
    Instead, just propagate the number of used LRs from the nested code
    into the L0 context, and be done with it. Should this become a burden,
    it can be easily rectified.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/vgic/vgic-v3-nested.c
+++ arch/arm64/kvm/vgic/vgic-v3-nested.c
@@ -294,6 +294,12 @@ void vgic_v3_load_nested(struct kvm_vcpu *vcpu)
 	__vgic_v3_activate_traps(cpu_if);
 
 	__vgic_v3_restore_state(cpu_if);
+
+	/*
+	 * Propagate the number of used LRs for the benefit of the HYP
+	 * GICv3 emulation code. Yes, this is a pretty sorry hack.
+	 */
+	vcpu->arch.vgic_cpu.vgic_v3.used_lrs = cpu_if->used_lrs;
 }
 
 void vgic_v3_put_nested(struct kvm_vcpu *vcpu)
@@ -326,6 +332,8 @@ void vgic_v3_put_nested(struct kvm_vcpu *vcpu)
 		__vcpu_sys_reg(vcpu, ICH_LRN(i)) = val;
 		s_cpu_if->vgic_lr[i] = 0;
 	}
+
+	vcpu->arch.vgic_cpu.vgic_v3.used_lrs = 0;
 }
 
 /*

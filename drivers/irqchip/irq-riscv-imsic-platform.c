3369643ba70a54bfc44f7c0315e36c2ac179c29f
Marc Zyngier <maz@kernel.org>
2024-12-04 09:37:42 +0000

    irqchip/riscv-imsic: Convert to msi_create_parent_irq_domain() helper
    Now that we have a concise helper to create an MSI parent domain,
    switch the RISC-V letter soup over to that.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- drivers/irqchip/irq-riscv-imsic-platform.c
+++ drivers/irqchip/irq-riscv-imsic-platform.c
@@ -325,16 +325,15 @@ int imsic_irqdomain_init(void)
 	}
 
 	/* Create Base IRQ domain */
-	imsic->base_domain = irq_domain_create_tree(imsic->fwnode,
-						    &imsic_base_domain_ops, imsic);
+	imsic->base_domain = msi_create_parent_irq_domain(imsic->fwnode,
+							  &imsic_msi_parent_ops,
+							  &imsic_base_domain_ops,
+							  0, 0, imsic, NULL);
+	
 	if (!imsic->base_domain) {
 		pr_err("%pfwP: failed to create IMSIC base domain\n", imsic->fwnode);
 		return -ENOMEM;
 	}
-	imsic->base_domain->flags |= IRQ_DOMAIN_FLAG_MSI_PARENT;
-	imsic->base_domain->msi_parent_ops = &imsic_msi_parent_ops;
-
-	irq_domain_update_bus_token(imsic->base_domain, DOMAIN_BUS_NEXUS);
 
 	global = &imsic->global;
 	pr_info("%pfwP:  hart-index-bits: %d,  guest-index-bits: %d\n",

d30df5d1069535b6c71f78b3867c38c45e2f51a5
Marc Zyngier <maz@kernel.org>
2025-02-20 13:48:55 +0000

    KVM: arm64: Hide ID_AA64MMFR2_EL1.NV from guest and userspace
    Since our take on FEAT_NV is to only support FEAT_NV2, we should
    never expose ID_AA64MMFR2_EL1.NV to a guest nor userspace.
    
    Make sure we mask this field for good.
    
    But as usual, we messed up the first place, and exposed the host
    value of that field to userspace. So on top of hidding it, we need
    to ignore any write that would otherwise match the host value.
    
    See the sorry MPAM story for additional context...
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -1627,6 +1627,7 @@ static u64 __kvm_read_sanitised_id_reg(const struct kvm_vcpu *vcpu,
 		break;
 	case SYS_ID_AA64MMFR2_EL1:
 		val &= ~ID_AA64MMFR2_EL1_CCIDX_MASK;
+		val &= ~ID_AA64MMFR2_EL1_NV;
 		break;
 	case SYS_ID_AA64MMFR3_EL1:
 		val &= ID_AA64MMFR3_EL1_TCRX | ID_AA64MMFR3_EL1_S1POE |
@@ -1941,6 +1942,18 @@ static int set_id_aa64pfr1_el1(struct kvm_vcpu *vcpu,
 {
 	/* See set_id_aa64pfr0_el1 for comment about MPAM */
 	filter_field(user_val, ID_AA64PFR1_EL1, MPAM_frac);
+
+	return set_id_reg(vcpu, rd, user_val);
+}
+
+static int set_id_aa64mmfr2_el1(struct kvm_vcpu *vcpu,
+				const struct sys_reg_desc *rd, u64 user_val)
+{
+	/*
+	 * We made the mistake to expose the now deprecated NV field,
+	 * so allow userspace to write it, but silently ignore it.
+	 */
+	filter_field(user_val, ID_AA64MMFR2_EL1, NV);
 
 	return set_id_reg(vcpu, rd, user_val);
 }
@@ -2671,7 +2684,8 @@ static const struct sys_reg_desc sys_reg_descs[] = {
 					ID_AA64MMFR1_EL1_XNX |
 					ID_AA64MMFR1_EL1_VH |
 					ID_AA64MMFR1_EL1_VMIDBits)),
-	ID_WRITABLE(ID_AA64MMFR2_EL1, ~(ID_AA64MMFR2_EL1_RES0 |
+	ID_FILTERED(ID_AA64MMFR2_EL1,
+		    id_aa64mmfr2_el1, ~(ID_AA64MMFR2_EL1_RES0 |
 					ID_AA64MMFR2_EL1_EVT |
 					ID_AA64MMFR2_EL1_FWB |
 					ID_AA64MMFR2_EL1_IDS |

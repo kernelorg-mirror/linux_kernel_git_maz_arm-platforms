2cbf999e93e9bfb3ef8a63f7cb232e6283e7b307
Marc Zyngier <maz@kernel.org>
2025-04-20 09:46:07 +0100

    of/irq: Add IRQ affinity reporting interface
    Plug the irq_populate_fwspec_info() helper into the OF layer
    to offer an IRQ affinity reporting function.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- drivers/of/irq.c
+++ drivers/of/irq.c
@@ -479,6 +479,26 @@ out:
 }
 EXPORT_SYMBOL_GPL(of_irq_get);
 
+const struct cpumask *of_irq_get_affinity(struct device_node *dev, int index)
+{
+	int rc;
+	struct of_phandle_args oirq;
+	struct irq_fwspec_info info;
+
+	rc = of_irq_parse_one(dev, index, &oirq);
+	if (rc)
+		return NULL;
+
+	of_phandle_args_to_fwspec(oirq.np, oirq.args, oirq.args_count,
+				  &info.fwspec);
+
+	if (!irq_populate_fwspec_info(&info) &&
+	    info.flags & IRQ_FWSPEC_INFO_AFFINITY_VALID)
+		return info.affinity;
+
+	return NULL;
+}
+
 /**
  * of_irq_get_byname - Decode a node's IRQ and return it as a Linux IRQ number
  * @dev: pointer to device tree node

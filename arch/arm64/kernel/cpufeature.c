2f4b39e994ecc2d7820ea1742e23da4218a0c70f
Colton Lewis <coltonlewis@google.com>
2025-07-14 22:58:55 +0000

    arm64: cpufeature: Add cpucap for HPMN0
    Add a capability for FEAT_HPMN0, whether MDCR_EL2.HPMN can specify 0
    counters reserved for the guest.
    
    This required changing HPMN0 to an UnsignedEnum in tools/sysreg
    because otherwise not all the appropriate macros are generated to add
    it to arm64_cpu_capabilities_arm64_features.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Colton Lewis <coltonlewis@google.com>
    Acked-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Link: https://patch.msgid.link/20250714225917.1396543-2-coltonlewis@google.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -554,6 +554,7 @@ static const struct arm64_ftr_bits ftr_id_mmfr0[] = {
 };
 
 static const struct arm64_ftr_bits ftr_id_aa64dfr0[] = {
+	ARM64_FTR_BITS(FTR_HIDDEN, FTR_STRICT, FTR_LOWER_SAFE, ID_AA64DFR0_EL1_HPMN0_SHIFT, 4, 0),
 	S_ARM64_FTR_BITS(FTR_HIDDEN, FTR_STRICT, FTR_LOWER_SAFE, ID_AA64DFR0_EL1_DoubleLock_SHIFT, 4, 0),
 	ARM64_FTR_BITS(FTR_HIDDEN, FTR_NONSTRICT, FTR_LOWER_SAFE, ID_AA64DFR0_EL1_PMSVer_SHIFT, 4, 0),
 	ARM64_FTR_BITS(FTR_HIDDEN, FTR_STRICT, FTR_LOWER_SAFE, ID_AA64DFR0_EL1_CTX_CMPs_SHIFT, 4, 0),
@@ -2896,6 +2897,13 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.capability = ARM64_HAS_FGT2,
 		.matches = has_cpuid_feature,
 		ARM64_CPUID_FIELDS(ID_AA64MMFR0_EL1, FGT, FGT2)
+	},
+	{
+		.desc = "HPMN0",
+		.type = ARM64_CPUCAP_SYSTEM_FEATURE,
+		.capability = ARM64_HAS_HPMN0,
+		.matches = has_cpuid_feature,
+		ARM64_CPUID_FIELDS(ID_AA64DFR0_EL1, HPMN0, IMP)
 	},
 #ifdef CONFIG_ARM64_SME
 	{

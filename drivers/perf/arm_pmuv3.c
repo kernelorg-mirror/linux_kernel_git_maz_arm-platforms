e0594e87350b468ac20f72af503c909e84e32762
Colton Lewis <coltonlewis@google.com>
2025-07-14 22:58:59 +0000

    perf: arm_pmuv3: Generalize counter bitmasks
    The OVSR bitmasks are valid for enable and interrupt registers as well as
    overflow registers. Generalize the names.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Colton Lewis <coltonlewis@google.com>
    Link: https://patch.msgid.link/20250714225917.1396543-6-coltonlewis@google.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- drivers/perf/arm_pmuv3.c
+++ drivers/perf/arm_pmuv3.c
@@ -545,7 +545,7 @@ static u64 armv8pmu_pmcr_n_read(void)
 
 static int armv8pmu_has_overflowed(u64 pmovsr)
 {
-	return !!(pmovsr & ARMV8_PMU_OVERFLOWED_MASK);
+	return !!(pmovsr & ARMV8_PMU_CNT_MASK_ALL);
 }
 
 static int armv8pmu_counter_has_overflowed(u64 pmnc, int idx)
@@ -781,7 +781,7 @@ static u64 armv8pmu_getreset_flags(void)
 	value = read_pmovsclr();
 
 	/* Write to clear flags */
-	value &= ARMV8_PMU_OVERFLOWED_MASK;
+	value &= ARMV8_PMU_CNT_MASK_ALL;
 	write_pmovsclr(value);
 
 	return value;

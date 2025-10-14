1989b7a604d4d88fa76f41693235aefb8e80769c
Sascha Bischoff <sascha.bischoff@arm.com>
2025-10-14 17:27:59 +0100

    KVM: arm64: gic-v5: Add KVM_DEV_TYPE_ARM_VGIC_V5_ITS UAPI
    Add the KVM_DEV_TYPE_ARM_VGIC_V5_ITS for creating the GICv5 ITS, and
    also KVM_VGIC_V5_ADDR_TYPE_ITS for setting the address of the guest's
    ITS, to the userspace API.

--- arch/arm64/include/uapi/asm/kvm.h
+++ arch/arm64/include/uapi/asm/kvm.h
@@ -99,8 +99,10 @@ struct kvm_regs {
 
 /* Supported VGICv5 address types  */
 #define KVM_VGIC_V5_ADDR_TYPE_IRS	6
+#define KVM_VGIC_V5_ADDR_TYPE_ITS	7
 
 #define KVM_VGIC_V5_IRS_SIZE		(2 * SZ_64K)
+#define KVM_VGIC_V5_ITS_SIZE		(2 * SZ_64K)
 
 #define KVM_ARM_VCPU_POWER_OFF		0 /* CPU is started in OFF state */
 #define KVM_ARM_VCPU_EL1_32BIT		1 /* CPU running a 32bit VM */

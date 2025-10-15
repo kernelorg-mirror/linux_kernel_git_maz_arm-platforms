9cd4fbea0f662029564413521c0e8d42c428c05f
Sascha Bischoff <sascha.bischoff@arm.com>
2025-10-15 10:08:57 +0100

    KVM: arm64: gic-v5: Add ITS IODEV to MMIO IF
    Extend the existing vgic MMIO interfaces to support the GICv5 ITS
    accesses for reads and writes.

--- arch/arm64/kvm/vgic/vgic-mmio.c
+++ arch/arm64/kvm/vgic/vgic-mmio.c
@@ -1052,6 +1052,9 @@ static int dispatch_mmio_read(struct kvm_vcpu *vcpu, struct kvm_io_device *dev,
 	case IODEV_GICV5_IRS:
 		data = region->read(vcpu, addr, len);
 		break;
+	case IODEV_GICV5_ITS:
+		data = region->its_read(vcpu->kvm, iodev->v5its, addr, len);
+		break;
 	}
 
 	vgic_data_host_to_mmio_bus(val, len, data);
@@ -1084,6 +1087,9 @@ static int dispatch_mmio_write(struct kvm_vcpu *vcpu, struct kvm_io_device *dev,
 		break;
 	case IODEV_GICV5_IRS:
 		region->write(vcpu, addr, len, data);
+		break;
+	case IODEV_GICV5_ITS:
+		region->its_write(vcpu->kvm, iodev->v5its, addr, len, data);
 		break;
 	}
 
--- arch/arm64/kvm/vgic/vgic-mmio.h
+++ arch/arm64/kvm/vgic/vgic-mmio.h
@@ -218,6 +218,7 @@ unsigned int vgic_v2_init_cpuif_iodev(struct vgic_io_device *dev);
 unsigned int vgic_v3_init_dist_iodev(struct vgic_io_device *dev);
 
 unsigned int vgic_v5_init_irs_iodev(struct vgic_io_device *dev);
+unsigned int vgic_v5_init_its_iodev(struct vgic_io_device *dev);
 
 u64 vgic_sanitise_outer_cacheability(u64 reg);
 u64 vgic_sanitise_inner_cacheability(u64 reg);

d29f075bde76b397b2af87d8f5d6e21337670451
Marc Zyngier <maz@kernel.org>
2026-07-13 17:01:16 +0100

    KVM: arm64: vgic-v3: Kill kvm_vgic_global_state.ich_vtr_el2
    kvm_vgic_global_state.ich_vtr_el2 is the last bit of caching that
    we can get rid of. Not as bad as a sysreg access, but still worse
    than a constant.
    
    Move over to the inlined stuff and remove the cached value.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/vgic/vgic.h
+++ arch/arm64/kvm/vgic/vgic.h
@@ -71,17 +71,6 @@
 				 ICH_VTR_EL2_IDbits)
 #define KVM_ICH_VTR_EL2_RES1	ICH_VTR_EL2_nV4
 
-static inline u64 kvm_get_guest_vtr_el2(void)
-{
-	u64 vtr;
-
-	vtr  = kvm_vgic_global_state.ich_vtr_el2;
-	vtr &= ~KVM_ICH_VTR_EL2_RES0;
-	vtr |= KVM_ICH_VTR_EL2_RES1;
-
-	return vtr;
-}
-
 /*
  * As per Documentation/virt/kvm/devices/arm-vgic-its.rst,
  * below macros are defined for ITS table entry encoding.
@@ -193,6 +182,17 @@ static inline u64 vgic_ich_vtr(void)
 				    ARM64_ALWAYS_SYSTEM,
 				    kvm_patch_ich_vtr_el2)
 		     : "=r" (vtr));
+
+	return vtr;
+}
+
+static inline u64 kvm_get_guest_vtr_el2(void)
+{
+	u64 vtr;
+
+	vtr  = vgic_ich_vtr();
+	vtr &= ~KVM_ICH_VTR_EL2_RES0;
+	vtr |= KVM_ICH_VTR_EL2_RES1;
 
 	return vtr;
 }
